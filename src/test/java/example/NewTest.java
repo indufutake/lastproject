4c75576030008272928a975a3bf1d083530e0485
indufutake <indufutake@gmail.com>
2018-10-30 13:29:41 +0530

    frist

---
+++ src/test/java/example/NewTest.java
@@ -0,0 +1,29 @@
+package example;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+
+public class NewTest {
+	
+	    private WebDriver driver;	
+	    
+		@BeforeTest
+		public void beforeTest() {	
+		    driver = new FirefoxDriver();  
+		}	
+		@Test				
+		public void testEasy() {	
+			driver.get("http://demo.guru99.com/test/guru99home/");  
+			String title = driver.getTitle();				 
+			Assert.assertTrue(title.contains("Demo Guru99 Page")); 		
+		}	
+		
+		@AfterTest
+		public void afterTest() {
+			driver.quit();			
+		}		
+}
